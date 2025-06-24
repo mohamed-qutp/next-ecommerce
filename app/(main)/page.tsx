62d289a5526b008b106991d8bdc11d1eaa9926fe
mohamed-qutp <m.qotb@sitksa-eg.com>
2025-06-24 11:19:39 +0300

    install next js to start E-commerce app

---
+++ app/(main)/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">Home Page</div>
+  )
+}
